0b438b0c8a5a0d5f3d2b2c0e5c23123132fddc86
Kre4 <denhol2002@gmail.com>
2023-10-24 22:25:07 +0300

    lab1: add simple test

---
+++ src/test/kotlin/ru/quipy/ProjectAggregateState.kt
@@ -0,0 +1,50 @@
+package ru.quipy
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.find
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import ru.quipy.api.ProjectAggregate
+import ru.quipy.core.EventSourcingService
+import ru.quipy.logic.ProjectAggregateState
+import ru.quipy.logic.create
+import java.util.*
+
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class ProjectAggregateState {
+    companion object {
+        private val testId = UUID.randomUUID()
+        private val userId = UUID.randomUUID()
+    }
+
+    @Autowired
+    private lateinit var projectEsService: EventSourcingService<UUID, ProjectAggregate, ProjectAggregateState>
+
+    @Autowired
+    lateinit var mongoTemplate: MongoTemplate
+
+    @BeforeEach
+    fun init() {
+        mongoTemplate.remove(Query.query(Criteria.where("aggregateId").`is`(testId)), "accounts")
+        mongoTemplate.remove(Query.query(Criteria.where("_id").`is`(testId)), "snapshots")
+        println(mongoTemplate.findAll(ProjectAggregate::class.java))
+    }
+
+    @Test
+    fun createProject() {
+        projectEsService.create {
+            it.create(testId, "test name", userId.toString());
+        }
+        val state = projectEsService.getState(testId)
+        Assertions.assertEquals(testId, state?.getId());
+    }
+}
