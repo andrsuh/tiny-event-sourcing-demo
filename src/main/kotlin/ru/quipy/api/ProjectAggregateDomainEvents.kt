56ab2caae1058c83064e28ff05f27da5777069e6
настя <75230747+enestesea@users.noreply.github.com>
2023-10-24 12:07:15 +0300

    feat: Project and User Aggregate Domain Events

--- src/main/kotlin/ru/quipy/api/ProjectAggregateDomainEvents.kt
+++ src/main/kotlin/ru/quipy/api/ProjectAggregateDomainEvents.kt
@@ -4,52 +4,64 @@ import ru.quipy.core.annotations.DomainEvent
 import ru.quipy.domain.Event
 import java.util.*
 
+
 const val PROJECT_CREATED_EVENT = "PROJECT_CREATED_EVENT"
+const val PARTICIPANT_ADDED_EVENT = "PARTICIPANT_ADDED_EVENT"
+const val LEAVE_PROJECT_EVENT = "LEAVE_PROJECT_EVENT"
 const val STATUS_CREATED_EVENT = "STATUS_CREATED_EVENT"
-const val STATUS_ASSIGNED_TO_TASK_EVENT = "STATUS_ASSIGNED_TO_TASK_EVENT"
-const val TASK_CREATED_EVENT = "TASK_CREATED_EVENT"
+const val STATUS_DELETED_EVENT = "STATUS_DELETED_EVENT"
+
 
 // API
 @DomainEvent(name = PROJECT_CREATED_EVENT)
 class ProjectCreatedEvent(
-    val projectId: UUID,
-    val title: String,
-    val creatorId: String,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val projectName: String,
+        val creatorId: String,
+        val description: String,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = PROJECT_CREATED_EVENT,
-    createdAt = createdAt,
+        name = PROJECT_CREATED_EVENT,
+        createdAt = createdAt,
+)
+
+@DomainEvent(name = PARTICIPANT_ADDED_EVENT)
+class ParticipantAddedEvent(
+        val projectId: UUID,
+        val userId: UUID,
+        createdAt: Long = System.currentTimeMillis(),
+) : Event<ProjectAggregate>(
+        name = PARTICIPANT_ADDED_EVENT,
+        createdAt = createdAt,
+)
+
+@DomainEvent(name = LEAVE_PROJECT_EVENT)
+class LeaveProjectEvent(
+        val projectId: UUID,
+        val userId: UUID,
+        createdAt: Long = System.currentTimeMillis(),
+) : Event<ProjectAggregate>(
+        name = LEAVE_PROJECT_EVENT,
+        createdAt = createdAt,
 )
 
 @DomainEvent(name = STATUS_CREATED_EVENT)
 class StatusCreatedEvent(
-    val projectId: UUID,
-    val statusId: UUID,
-    val statusName: String,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val statusId: UUID,
+        val statusName: String,
+        val colour: String,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = STATUS_CREATED_EVENT,
-    createdAt = createdAt,
+        name = STATUS_CREATED_EVENT,
+        createdAt = createdAt,
 )
 
-@DomainEvent(name = TASK_CREATED_EVENT)
-class TaskCreatedEvent(
-    val projectId: UUID,
-    val taskId: UUID,
-    val taskName: String,
-    createdAt: Long = System.currentTimeMillis(),
-) : Event<ProjectAggregate>(
-    name = TASK_CREATED_EVENT,
-    createdAt = createdAt
-)
-
-@DomainEvent(name = STATUS_ASSIGNED_TO_TASK_EVENT)
+@DomainEvent(name = STATUS_DELETED_EVENT)
 class StatusAssignedToTaskEvent(
-    val projectId: UUID,
-    val taskId: UUID,
-    val statusId: UUID,
-    createdAt: Long = System.currentTimeMillis(),
+        val statusId: UUID,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = STATUS_ASSIGNED_TO_TASK_EVENT,
-    createdAt = createdAt
+        name = STATUS_DELETED_EVENT,
+        createdAt = createdAt
 )
--- src/main/kotlin/ru/quipy/api/UserAggregateDomainEvents.kt
+++ src/main/kotlin/ru/quipy/api/UserAggregateDomainEvents.kt
@@ -1,2 +1,20 @@
 package ru.quipy.api
 
+import ru.quipy.core.annotations.DomainEvent
+import ru.quipy.domain.Event
+import java.util.*
+
+const val USER_CREATED_EVENT = "USER_CREATED_EVENT"
+// API
+
+@DomainEvent(name = USER_CREATED_EVENT)
+class UserCreatedEvent(
+        val userId: UUID,
+        val nickname: String,
+        val userName: String,
+        val password: String,
+        createdAt: Long = System.currentTimeMillis(),
+) : Event<ProjectAggregate>(
+        name = USER_CREATED_EVENT,
+        createdAt = createdAt
+)
