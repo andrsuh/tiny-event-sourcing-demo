b4ee9a20d53276fa643ab61c44ac50894c481cc7
Kre4 <denhol2002@gmail.com>
2023-10-26 16:18:40 +0300

    lab1: add commands

--- src/main/kotlin/ru/quipy/logic/ProjectAggregateCommands.kt
+++ src/main/kotlin/ru/quipy/logic/ProjectAggregateCommands.kt
@@ -1,9 +1,7 @@
 package ru.quipy.logic
 
-import ru.quipy.api.ProjectCreatedEvent
-import ru.quipy.api.TagAssignedToTaskEvent
-import ru.quipy.api.TagCreatedEvent
-import ru.quipy.api.TaskCreatedEvent
+import org.springframework.util.StringUtils
+import ru.quipy.api.*
 import java.util.*
 
 
@@ -18,25 +16,64 @@ fun ProjectAggregateState.create(id: UUID, title: String, creatorId: String): Pr
     )
 }
 
+fun ProjectAggregateState.addStatus(name: String, color: String): StatusCreatedEvent {
+    require(StringUtils.hasText(name)) { "Name cannot be empty" }
+    require(StringUtils.hasText(color)) { "Color cannot be empty" }
+    require(Regex("^#(?:[0-9a-fA-F]{3}){1,2}\$").matches(color)) { "Color must be in #hex format" }
+    return StatusCreatedEvent(
+        projectId = this.getId(),
+        statusId = UUID.randomUUID(),
+        statusName = name,
+        statusColor = color
+    )
+}
+
+fun ProjectAggregateState.deleteStatus(projectId: UUID, statusId: UUID): StatusDeletedEvent {
+    require(this.getId() == projectId)
+    if (this.tasks.values.find { task -> task.status == statusId } == null) {
+        projectStatuses.remove(statusId)
+        return StatusDeletedEvent(
+            projectId = projectId,
+            statusId = statusId
+        )
+    } else
+        throw IllegalArgumentException("Task with status id $statusId exists")
+}
+
+fun ProjectAggregateState.assignStatus(projectId: UUID, taskId: UUID, statusId: UUID): StatusAssignedToTaskEvent {
+    require(this.getId() == projectId)
+    require(!projectStatuses.containsKey(statusId)) { "Status doesn't exists: $statusId" }
+    require(!tasks.containsKey(taskId)) { "Task doesn't exists: $taskId" }
+    return StatusAssignedToTaskEvent(
+        projectId = projectId,
+        taskId = taskId,
+        statusId = statusId
+    )
+}
+
 fun ProjectAggregateState.addTask(name: String): TaskCreatedEvent {
+    require(StringUtils.hasText(name)) { "Name cannot be empty" }
     return TaskCreatedEvent(projectId = this.getId(), taskId = UUID.randomUUID(), taskName = name)
 }
 
-fun ProjectAggregateState.createTag(name: String): TagCreatedEvent {
-    if (projectTags.values.any { it.name == name }) {
-        throw IllegalArgumentException("Tag already exists: $name")
-    }
-    return TagCreatedEvent(projectId = this.getId(), tagId = UUID.randomUUID(), tagName = name)
+fun ProjectAggregateState.renameTask(projectId: UUID, taskId: UUID, newName: String): TaskRenamedEvent {
+    require(this.getId() == projectId)
+    require(StringUtils.hasText(newName)) { "New name cannot be empty" }
+    require(!tasks.containsKey(taskId)) { "Task doesn't exists: $taskId" }
+    return TaskRenamedEvent(
+        projectId = projectId,
+        taskId = taskId,
+        newName = newName
+    )
 }
 
-fun ProjectAggregateState.assignTagToTask(tagId: UUID, taskId: UUID): TagAssignedToTaskEvent {
-    if (!projectTags.containsKey(tagId)) {
-        throw IllegalArgumentException("Tag doesn't exists: $tagId")
-    }
-
-    if (!tasks.containsKey(taskId)) {
-        throw IllegalArgumentException("Task doesn't exists: $taskId")
-    }
-
-    return TagAssignedToTaskEvent(projectId = this.getId(), tagId = tagId, taskId = taskId)
+fun ProjectAggregateState.assignUser(projectId: UUID, taskId: UUID, userId: UUID): UserAssignedToTaskEvent {
+    require(this.getId() == projectId)
+    require(!tasks.containsKey(taskId)) { "Task doesn't exists: $taskId" }
+    // TODO check if user exists in project
+    return UserAssignedToTaskEvent(
+        projectId = projectId,
+        taskId = taskId,
+        userId = userId
+    )
 }
