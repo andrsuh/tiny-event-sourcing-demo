aa643ea8b359488aed51ffe9a49c12d96a1a5e87
Kre4 <denhol2002@gmail.com>
2023-10-26 15:14:43 +0300

    lab1: rename test

--- src/test/kotlin/ru/quipy/ProjectAggregateStateTest.kt
+++ src/test/kotlin/ru/quipy/ProjectAggregateStateTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.find
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.test.annotation.DirtiesContext
@@ -20,10 +19,11 @@ import java.util.*
 @SpringBootTest
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-class ProjectAggregateState {
+class ProjectAggregateStateTest {
     companion object {
         private val testId = UUID.randomUUID()
         private val userId = UUID.randomUUID()
+        private val testProjectName = "testProjectName$541(*@#&91459"
     }
 
     @Autowired
@@ -42,7 +42,7 @@ class ProjectAggregateState {
     @Test
     fun createProject() {
         projectEsService.create {
-            it.create(testId, "test name", userId.toString());
+            it.create(testId, testProjectName, userId.toString());
         }
         val state = projectEsService.getState(testId)
         Assertions.assertEquals(testId, state?.getId());
