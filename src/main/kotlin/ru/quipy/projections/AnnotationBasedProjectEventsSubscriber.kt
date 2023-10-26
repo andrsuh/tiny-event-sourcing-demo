0010ae369e8c38620bba698987ca9913ddbccd72
Kre4 <denhol2002@gmail.com>
2023-10-26 18:40:18 +0300

    lab1: delete unused event

--- src/main/kotlin/ru/quipy/projections/AnnotationBasedProjectEventsSubscriber.kt
+++ src/main/kotlin/ru/quipy/projections/AnnotationBasedProjectEventsSubscriber.kt
@@ -4,7 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import ru.quipy.api.ProjectAggregate
-import ru.quipy.api.TagCreatedEvent
+import ru.quipy.api.ProjectCreatedEvent
 import ru.quipy.api.TaskCreatedEvent
 import ru.quipy.streams.annotation.AggregateSubscriber
 import ru.quipy.streams.annotation.SubscribeEvent
@@ -22,8 +22,12 @@ class AnnotationBasedProjectEventsSubscriber {
         logger.info("Task created: {}", event.taskName)
     }
 
+    //TODO: add subscribers if needed
+
     @SubscribeEvent
-    fun tagCreatedSubscriber(event: TagCreatedEvent) {
-        logger.info("Tag created: {}", event.tagName)
+    fun projectCreated(event: ProjectCreatedEvent) {
+        logger.info("PROJECT created: {}", event.title)
     }
+
+    //TODO: перехват нужен только тогда, когда данные нужны для другого контекста/из другого контекста?
 }
