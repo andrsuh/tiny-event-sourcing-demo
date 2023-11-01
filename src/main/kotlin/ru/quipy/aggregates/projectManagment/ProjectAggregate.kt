8bb936e3a04e35304d440cad36a0947c257f8195
ClwnYeti <crazyyeti@mail.ru>
2023-11-01 22:26:21 +0300

    feat: aggregates created

---
+++ src/main/kotlin/ru/quipy/aggregates/projectManagment/ProjectAggregate.kt
@@ -0,0 +1,8 @@
+package ru.quipy.aggregates.projectManagment
+
+import ru.quipy.core.annotations.AggregateType
+import ru.quipy.domain.Aggregate
+
+@AggregateType(aggregateEventsTableName = "projects")
+class ProjectAggregate(
+) : Aggregate
