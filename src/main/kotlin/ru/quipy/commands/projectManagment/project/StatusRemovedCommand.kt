da7e6689ffb4f78d18d905fb7ba4b37058a0b6f7
ClwnYeti <crazyyeti@mail.ru>
2023-11-04 12:12:13 +0300

    feat: project commands added

---
+++ src/main/kotlin/ru/quipy/commands/projectManagment/project/StatusRemovedCommand.kt
@@ -0,0 +1,15 @@
+package ru.quipy.commands.projectManagment.project
+
+import ru.quipy.events.projectManagment.project.StatusRemovedEvent
+import ru.quipy.states.projectManagment.ProjectAggregateState
+import java.util.UUID
+
+fun ProjectAggregateState.removeStatus(statusId: UUID) : StatusRemovedEvent {
+    if (!this.statuses.containsKey(statusId)) {
+        throw IllegalArgumentException("Project doesn't have status with id $statusId")
+    }
+    return StatusRemovedEvent(
+        this.getId(),
+        statusId,
+    )
+}
