ec5c5554a242f3f2d29a86334472f2856e6cc9be
ClwnYeti <crazyyeti@mail.ru>
2023-11-04 12:11:37 +0300

    fix: codestyle issues

--- src/main/kotlin/ru/quipy/events/projectManagment/project/StatusAddedEvent.kt
+++ src/main/kotlin/ru/quipy/events/projectManagment/project/StatusAddedEvent.kt
@@ -4,14 +4,14 @@ import ru.quipy.api.ProjectAggregate
 import ru.quipy.core.annotations.DomainEvent
 import ru.quipy.domain.Event
 import java.awt.Color
-import java.util.*
+import java.util.UUID
 
 const val STATUS_ADDED_EVENT = "STATUS_ADDED_EVENT"
 
 @DomainEvent(name = STATUS_ADDED_EVENT)
 class StatusAddedEvent(
-    val statusId: UUID,
     val projectId: UUID,
+    val statusId: UUID,
     val statusName: String,
     val color: Color,
     createdAt: Long = System.currentTimeMillis(),
