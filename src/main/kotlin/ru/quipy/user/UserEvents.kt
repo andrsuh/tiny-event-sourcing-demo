999a71c9ce600a84dd0bd8d17b7bbb749db41aa3
Dmitry.Leshchikov <sliceofkekus@icloud.com>
2023-11-14 19:56:37 +0300

    Slightly modified user-specific events.

--- src/main/kotlin/ru/quipy/user/UserEvents.kt
+++ src/main/kotlin/ru/quipy/user/UserEvents.kt
@@ -5,8 +5,6 @@ import ru.quipy.domain.Event
 import ru.quipy.user.dto.UserModel
 import ru.quipy.user.eda.api.UserAggregate
 
-import java.util.*
-
 const val USER_CREATED_EVENT = "USER_CREATED_EVENT"
 const val USER_DELETED_EVENT = "USER_DELETED_EVENT"
 
