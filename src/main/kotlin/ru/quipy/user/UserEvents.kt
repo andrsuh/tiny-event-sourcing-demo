93b3864049995fc34dd48900aed5817429e74654
Dmitry.Leshchikov <sliceofkekus@icloud.com>
2023-11-14 19:56:24 +0300

    Slightly modified user-specific events.

--- src/main/kotlin/ru/quipy/user/UserEvents.kt
+++ src/main/kotlin/ru/quipy/user/UserEvents.kt
@@ -5,13 +5,15 @@ import ru.quipy.domain.Event
 import ru.quipy.user.dto.UserModel
 import ru.quipy.user.eda.api.UserAggregate
 
+import java.util.*
+
 const val USER_CREATED_EVENT = "USER_CREATED_EVENT"
 const val USER_DELETED_EVENT = "USER_DELETED_EVENT"
 
 @DomainEvent(name = USER_CREATED_EVENT)
-data class UserCreatedEvent(
+class UserCreatedEvent(
         val user: UserModel,
-        val createdAt: Long = System.currentTimeMillis()
+        createdAt: Long = System.currentTimeMillis()
 ) : Event<UserAggregate>(
         name = USER_CREATED_EVENT,
         createdAt = createdAt
@@ -20,7 +22,7 @@ data class UserCreatedEvent(
 @DomainEvent(name = USER_DELETED_EVENT)
 class UserDeletedEvent(
         val username: String,
-        val createdAt: Long = System.currentTimeMillis()
+        createdAt: Long = System.currentTimeMillis()
 ) : Event<UserAggregate>(
         name = USER_CREATED_EVENT,
         createdAt = createdAt
