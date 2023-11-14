b753ab603030be2ed8983f51c0d2e9a4d8d00c33
Dmitry.Leshchikov <sliceofkekus@icloud.com>
2023-11-14 19:52:59 +0300

    Slightly modified user-specific events.

--- src/main/kotlin/ru/quipy/user/UserEvents.kt
+++ src/main/kotlin/ru/quipy/user/UserEvents.kt
@@ -1,7 +1,26 @@
 package ru.quipy.user
 
+import ru.quipy.core.annotations.DomainEvent
 import ru.quipy.user.dto.UserModel
+import ru.quipy.user.eda.api.UserAggregate
 
-data class UserCreatedEvent(val user: UserModel)
+const val USER_CREATED_EVENT = "USER_CREATED_EVENT"
+const val USER_DELETED_EVENT = "USER_DELETED_EVENT"
 
-data class UserDeletedEvent(val username: String)
+@DomainEvent(name = USER_CREATED_EVENT)
+data class UserCreatedEvent(
+        val user: UserModel,
+        val createdAt: Long = System.currentTimeMillis()
+) : Event<UserAggregate>(
+        name = USER_CREATED_EVENT,
+        createdAt = createdAt
+)
+
+@DomainEvent(name = USER_DELETED_EVENT)
+class UserDeletedEvent(
+        val username: String,
+        val createdAt: Long = System.currentTimeMillis()
+) : Event<UserAggregate>(
+        name = USER_CREATED_EVENT,
+        createdAt = createdAt
+)
---
+++ src/main/kotlin/ru/quipy/user/controller/UserController.kt
@@ -0,0 +1,22 @@
+package ru.quipy.user.controller
+
+import org.springframework.web.bind.annotation.*
+import ru.quipy.user.UserService
+import ru.quipy.user.dto.UserLogin
+import ru.quipy.user.dto.UserModel
+import ru.quipy.user.dto.UserRegister
+
+@RestController
+@RequestMapping("/users")
+class UserController(private val userService: UserService) {
+
+    @PostMapping
+    fun register(@RequestBody request: UserRegister) = userService.createOne(request)
+
+    @GetMapping("/{username}")
+    fun getUser(@PathVariable username: String): UserModel =
+            userService.getOne(username)
+
+    @GetMapping("/login")
+    fun login(@RequestBody request: UserLogin): UserModel = userService.logIn(request)
+}
